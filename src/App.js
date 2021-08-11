f0bea0de0c1fbcb2f3e3002b9d92abfe69926254
Andrew Chua <andrewchua964@gmail.com>
2021-08-11 16:26:40 +0800

    Added comments to main file

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
+// Imports for React and React-Leaflet
 import { useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup, CircleMarker, Circle, useMapEvents, MapConsumer} from 'react-leaflet'
-import L from 'leaflet'
+import { MapContainer, TileLayer, Marker, Circle, MapConsumer} from 'react-leaflet'
 
+// Imports for the slider from ant.design
 import "antd/dist/antd.css";
 import { Slider } from 'antd';
 
+// Main app. 
 const App = () => {  
-  const [currentValue, setCurrentValue] = useState(0)
-  const [circleLoc, setCircleLoc] = useState([14.596699688557514, 120.97303782481114])  
-  const [circleRad, setCircleRad] = useState(400)
-  const [position, setPosition] = useState(null)  
+  const [currentValue, setCurrentValue] = useState(0) // useState hook for slider values
+  const [circleLoc, setCircleLoc] = useState([14.596699688557514, 120.97303782481114]) // useState hook for circle latlng
+  const [circleRad, setCircleRad] = useState(400) // useState hook for circle radius
+  const [position, setPosition] = useState(null) // useState hook for marker latlng
 
   return (
     <div>    
-    <MapContainer center = {[14.612822777072683, 120.94886916809494]} zoom = {15}>
+    <MapContainer center = {[14.600316439299878, 120.9874556853269]} zoom = {15}> {/* Container for the Map. Has starting position and zoom level passed in as props  */}
       <MapConsumer>
-        {(map) => {
-          map.whenReady(() => {            
-            map.locate()
+        {(map) => { 
+          map.whenReady(() => { // whenReady event runs when the map object is first loaded      
+            map.locate() // Locates the user's location. Will ask for permission from the user first.
 
-            map.on('locationfound', (e) => {
-              setPosition(e.latlng)
-              setCircleLoc(e.latlng)
-              map.flyTo(e.latlng, map.getZoom())
+            map.on('locationfound', (e) => { // Event that runs when map.locate() is succesful.
+              setPosition(e.latlng) // Sets marker position to user's location
+              setCircleLoc(e.latlng) // Sets circle's position to user's location
+              map.flyTo(e.latlng, map.getZoom()) // Pans the map from the initial location to the user's current location
             })
           })
           
-          return position === null ? null : (
-            <Marker
-              position={position}
+          return position === null ? null : ( // Ternary operator that checks if position is null
+            <Marker 
+              position={position} 
               draggable = {true}
               eventHandlers={{
-                drag: (e) => {
+                drag: (e) => { // Drag event listener. Will adjust circle's location to the marker's current location when dragged
                   setCircleLoc([e.latlng.lat, e.latlng.lng])                      
                 }
               }}
@@ -44,9 +46,10 @@ const App = () => {
       <TileLayer 
         url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
+      /> {/* TileLayer which contains the URL to the Tile provider + attribution */}
     </MapContainer> 
-
+    
+    {/* Slider from ant.design */}
     <div style={{
       display: 'block', width: 700, padding: 30
     }}>
